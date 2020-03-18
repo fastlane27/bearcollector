24eb819d44ab20c75d8013a49a2cf1f50219dac6
David Lane <fastlane27@gmail.com>
2020-03-18 16:28:33 -0500

    update bear model

--- main_app/models.py
+++ main_app/models.py
@@ -5,6 +5,7 @@ class Bear(models.Model):
     name = models.CharField(max_length=50)
     species = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
+    age = models.IntegerField()
 
     def __str__(self):
         return self.name
