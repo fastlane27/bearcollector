803a14da816f064ce02d526786174be87de6151a
David Lane <fastlane27@gmail.com>
2020-03-18 15:47:57 -0500

    bear index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('bears/', views.bears_index, name='bears_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Bear
 
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def bears_index(request):
+    bears = Bear.objects.all()
+    return render(request, 'bears/index.html', {'bears': bears})
